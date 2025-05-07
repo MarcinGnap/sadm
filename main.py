6b3d4de1c89b3aba14563d3b8b9a748c51f97f45
Marcin Gnap <80780114+MarcinGnap@users.noreply.github.com>
2025-05-07 23:57:12 +0200

    param with norm

--- main.py
+++ main.py
@@ -1,43 +1,33 @@
 import pandas as pd
 from src import par, nonpar, posthoc, logreg, statsanaly
+from scipy.stats import shapiro, normaltest, anderson
+
 
 if __name__ == "__main__":
 
     df = pd.read_csv('./data/ifood_df.csv')
     df2 = pd.read_csv('./data/Sales_without_NaNs_v1.3.csv')
 
-
-    print("\nDescriptive statistics:")
-    statsanaly.statsanaly(df)
+    # print("\nDescriptive statistics:")
+    # statsanaly.statsanaly(df)
 
     print("\nParametric tests:")
-    par.par_tStudent(df)
-    par.par_tStudentrel(df2)
+    par.allpar()
 
-    print("\nNonparametric tests:")
-    nonpar.nonpar_wilcoxon(df)
-    nonpar.nonpar_friedman(df)
+    # print("\nNonparametric tests:")
+    # nonpar.nonpar_wilcoxon(df)
+    # nonpar.nonpar_friedman(df)
+    #
+    # print("\nFiredman + post-hoc nemenyi test:")
+    # columns = [
+    #     "MntWines", "MntMeatProducts", "MntSweetProducts",
+    #     "MntFruits", "MntFishProducts", "MntGoldProds"
+    # ]
+    #
+    # labels = ["Wino", "Mięso", "Słodycze", "Owoce", "Ryby", "Złoto"]
+    # posthoc.friedman_nemenyi(df, columns, labels)
 
-    print("\nFiredman + post-hoc nemenyi test:")
-    columns = [
-        "MntWines", "MntMeatProducts", "MntSweetProducts",
-        "MntFruits", "MntFishProducts", "MntGoldProds"
-    ]
 
-    labels = ["Wino", "Mięso", "Słodycze", "Owoce", "Ryby", "Złoto"]
-    posthoc.friedman_nemenyi(df, columns, labels)
 
     # print("\nLogistic regression:")
     # logreg.logreg(df)
-
-    # married = df[df["marital_Married"] == 1]["Income"]
-    # single = df[df["marital_Single"] == 1]["Income"]
-    #
-    # stat, p_value = ttest_ind(married, single, equal_var=False)
-    #
-    # print(f"Test t-Studenta: Statystyka: = {stat:.5f}, p-value = {p_value:.5f}")
-    #
-    # if p_value < 0.05:
-    #     print("Istotna różnica w dochodach między osobami po ślubie a singlami")
-    # else:
-    #     print("Brak istotnej różnicy w dochodach między osobami po ślubie a singlami")
--- src/par.py
+++ src/par.py
@@ -1,4 +1,13 @@
 from scipy.stats import ttest_ind, ttest_rel
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from scipy.stats import levene, shapiro, ttest_ind
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 # def test_normal(df_s1: DataFrame, df_s2: DataFrame = None, col1: str = None, col2: str = None, fil: str = None,
 #                 dependent: bool = True, max_size_diff_ratio: float = 0.05):
@@ -34,38 +43,97 @@ from scipy.stats import ttest_ind, ttest_rel
 #
 #     return normal and equal_var and equal_length
 
-def par_tStudent(df):
-    print("\nT-Student ind:")
+# def par_tStudent(df):
+#     print("\nT-Student ind:")
+#
+#     df['InRelationship'] = (df['marital_Married'] + df['marital_Together']) > 0
+#
+#     married = df[df["marital_Married"] == 1]["Income"]
+#     single = df[df["InRelationship"] == 1]["Income"]
+#
+#     stat, p_value = ttest_ind(married, single, equal_var=False)
+#     print("Test t-Studenta dla danych niezależnych (przychód osób w związku vs singli):")
+#
+#     print(f"Statystyka: = {stat:.5f}, p-value = {p_value:}")
+#
+#     if p_value < 0.05:
+#         print("Istotna różnica w dochodach między osobami w relacji a singlami\n")
+#     else:
+#         print("Brak istotnej różnicy w dochodach między osobami w relacji a singlami\n")
+#
+#
+# def par_tStudentrel(df):
+#     print("\nT-Student rel:")
+#
+#     filtered = df[df['Purchase_Made'] == 'Yes'].dropna()
+#     before = filtered['Sales_Before']
+#     after = filtered['Sales_After']
+#
+#     stat, p = ttest_rel(before, after)
+#
+#     print("Test t-Studenta dla danych zależnych (ilość zakupionych towarów przez klienta przed i po interwencji):")
+#     print(f"t = {stat:.5f}, p = {p:}")
+#
+#     if p < 0.05:
+#         print("Różnica jest statystycznie istotna.")
+#     else:
+#         print("Brak istotnej różnicy.")
 
-    df['InRelationship'] = (df['marital_Married'] + df['marital_Together']) > 0
+def allpar():
+    iris = datasets.load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['species'] = iris.target
+    df['species'] = df['species'].map({0: 'setosa', 1: 'versicolor', 2: 'virginica'})
 
-    married = df[df["marital_Married"] == 1]["Income"]
-    single = df[df["InRelationship"] == 1]["Income"]
+    df.columns = [col.replace(" (cm)", "").replace(" ", "_") for col in df.columns]
 
-    stat, p_value = ttest_ind(married, single, equal_var=False)
-    print("Test t-Studenta dla danych niezależnych (przychód osób w związku vs singli):")
+    # anova
+    model = ols('sepal_width ~ C(species)', data=df).fit()
+    anova_table = sm.stats.anova_lm(model, typ=2)
 
-    print(f"Statystyka: = {stat:.5f}, p-value = {p_value:}")
+    # levene - jednorodność wariancji
+    levene_stat, levene_p = levene(
+        df[df['species'] == 'setosa']['sepal_width'],
+        df[df['species'] == 'versicolor']['sepal_width'],
+        df[df['species'] == 'virginica']['sepal_width']
+    )
 
-    if p_value < 0.05:
-        print("Istotna różnica w dochodach między osobami w relacji a singlami\n")
-    else:
-        print("Brak istotnej różnicy w dochodach między osobami w relacji a singlami\n")
+    # shapiro-wilka - norm
+    shapiro_stat, shapiro_p = shapiro(model.resid)
 
+    # Test post-hoc Tukey HSD
+    tukey = pairwise_tukeyhsd(endog=df['sepal_width'], groups=df['species'], alpha=0.05)
 
-def par_tStudentrel(df):
-    print("\nT-Student rel:")
+    plt.figure(figsize=(8, 5))
+    sns.boxplot(x='species', y='sepal_width', data=df)
+    plt.title('Rozkłady szerokości kielicha dla poszczególnych gatunków irysów')
+    plt.tight_layout()
+    plt.show()
 
-    filtered = df[df['Purchase_Made'] == 'Yes'].dropna()
-    before = filtered['Sales_Before']
-    after = filtered['Sales_After']
+    # t-welcha
+    setosa_len = df[df['species'] == 'setosa']['sepal_length']
+    versicolor_len = df[df['species'] == 'versicolor']['sepal_length']
 
-    stat, p = ttest_rel(before, after)
+    shapiro_setosa = shapiro(setosa_len)
+    shapiro_versicolor = shapiro(versicolor_len)
 
-    print("Test t-Studenta dla danych zależnych (ilość zakupionych towarów przez klienta przed i po interwencji):")
-    print(f"t = {stat:.5f}, p = {p:}")
+    # t-welcha różnice avg
+    welch_t_stat, welch_p_val = ttest_ind(setosa_len, versicolor_len, equal_var=False)
 
-    if p < 0.05:
-        print("Różnica jest statystycznie istotna.")
-    else:
-        print("Brak istotnej różnicy.")
+    plt.figure(figsize=(8, 5))
+    sns.boxplot(data=df[df['species'].isin(['setosa', 'versicolor'])], x='species', y='sepal_length')
+    plt.title('Porównanie rozkładów długości kielicha dla gatunków setosa i versicolor')
+    plt.tight_layout()
+    plt.show()
+
+    print("Test ANOVA")
+    print(anova_table)
+    print(f"\nLevene (homogeniczność wariancji): stat = {levene_stat:.4f}, p = {levene_p:.4f}")
+    print(f"Shapiro-Wilk (normalność reszt): stat = {shapiro_stat:.4f}, p = {shapiro_p:.4f}")
+    print("\n=== Post-hoc Tukey HSD ===")
+    print(tukey.summary())
+
+    print("\nTest T-Welcha")
+    print(f"Setosa – Shapiro: p = {shapiro_setosa.pvalue:.4f}")
+    print(f"Versicolor – Shapiro: p = {shapiro_versicolor.pvalue:.4f}")
+    print(f"T-Welcha: stat = {welch_t_stat:.4f}, p = {welch_p_val:.4e}")
